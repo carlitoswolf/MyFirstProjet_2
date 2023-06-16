41bb06d137d7ea7a9d6e0f3af7d1945d4683bc35
Carlos Cabrera <carlosoficial15@gmail.com>
2023-06-15 21:01:59 -0600

    Cambio realizado el 15/06/2023

--- app/src/main/java/com/example/myapplication/PhotoActivity.java
+++ app/src/main/java/com/example/myapplication/PhotoActivity.java
@@ -5,18 +5,26 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class PhotoActivity extends AppCompatActivity {
 
@@ -25,6 +33,8 @@ public class PhotoActivity extends AppCompatActivity {
 
     ImageView ObjectImage;
     Button btnTakePhoto;
+
+    String currentPhotoPath;
 
     String pathPhoto;
 
@@ -49,7 +59,8 @@ public class PhotoActivity extends AppCompatActivity {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, peticion_acceso_camara);
 
         } else {
-            TomarFoto();
+            dispatchTakePictureIntent();
+            //TomarFoto();
         }
     }
 
@@ -61,7 +72,8 @@ public class PhotoActivity extends AppCompatActivity {
         {
             if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED )
             {
-                TomarFoto();
+                dispatchTakePictureIntent();
+                //TomarFoto();
             }
             else
             {
@@ -85,9 +97,50 @@ public class PhotoActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == peticion_captura_imagen){
-            Bundle extras = data.getExtras();
-            Bitmap imagen = (Bitmap) extras.get("data");
-            ObjectImage.setImageBitmap(imagen);
+            ObjectImage.setImageURI(data.getData());
+
+//            Bundle extras = data.getExtras();
+//            Bitmap imagen = (Bitmap) extras.get("data");
+//            ObjectImage.setImageBitmap(imagen);
         }
     }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.myapplication.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, peticion_captura_imagen);
+            }
+        }
+    }
+
 }
